f3e8633f0cfd24819ef90f9b66bb23552c86db9f
Emmah-oo <emmaochonogor715@gmail.com>
2024-03-26 10:52:43 +0100

    feat: completed layout

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -62,12 +62,10 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
           </div>
         </Box>
         <Box className="overflow-y-auto h-full">
-            <Library />
+          <Library />
         </Box>
       </div>
-      <main className="h-full flex-1 overflow-y-auto py-2">
-        {children}
-      </main>
+      <main className="h-full flex-1 overflow-y-auto py-2">{children}</main>
     </div>
   );
 };
