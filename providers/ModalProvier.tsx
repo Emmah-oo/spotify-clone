a170145e12564fb0c8803d1fcfb2691cf08a5724
Emmah-oo <emmaochonogor715@gmail.com>
2024-03-27 10:05:17 +0100

    feat: built user and modal provider

---
+++ providers/ModalProvier.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+  return <>Modal</>;
+};
+
+export default ModalProvider;
