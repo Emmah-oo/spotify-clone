539c5421a6c483b7d518757037da0fabf3fe198d
Emmah-oo <emmaochonogor715@gmail.com>
2024-04-02 10:49:11 +0100

    feat: added songs to the homapage

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,9 +1,14 @@
+import getSongs from "@/actions/getSongs";
+
 import Header from "@/components/Header";
 import ListItem from "@/components/ListItem";
 
+import PageContent from "./components/PageContent";
+
 export const revalidate = 0; //makes this page not to be cached, the data would always be up to date
 
-export default function Home() {
+export default async function Home() {
+  const songs = await getSongs();
   return (
     <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
       <Header>
@@ -22,7 +27,9 @@ export default function Home() {
         <div className="flex justify-between items-center">
           <h1 className="text-white text-2xl font-semibold">Newest songs</h1>
         </div>
-        <div>List of Songs!</div>
+        <div>
+          <PageContent songs={songs} />
+        </div>
       </div>
     </div>
   );
