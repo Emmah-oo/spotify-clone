8f0329845cd7b4079bc4b555b7749d26a802e642
Emmah-oo <emmaochonogor715@gmail.com>
2024-04-02 10:07:50 +0100

    feat: created function to upload images to supabase bucket

--- components/UploadModal.tsx
+++ components/UploadModal.tsx
@@ -3,6 +3,7 @@
 import uniqid from "uniqid";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
 
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
@@ -19,6 +20,7 @@ const UploadModal = () => {
   const uploadModal = useUploadModal();
   const { user } = useUser();
   const supabaseClient = useSupabaseClient();
+  const router = useRouter();
 
   const { register, handleSubmit, reset } = useForm<FieldValues>({
     defaultValues: {
@@ -65,6 +67,40 @@ const UploadModal = () => {
       }
 
       //upload image
+      const { data: imageData, error: imageError } =
+        await supabaseClient.storage
+          .from("images")
+          .upload(`image-${values.title}-${uniqueID}`, imageFile, {
+            cacheControl: "3600",
+            upsert: false,
+          });
+
+      if (imageError) {
+        setisLoading(false);
+        return toast.error("Failed to upload image");
+      }
+
+      //if no error in image & song upload, add the file paths to the songs table in the database
+      const { error: supabaseError } = await supabaseClient
+        .from("songs")
+        .insert({
+          user_id: user.id,
+          title: values.title,
+          author: values.author,
+          image_path: imageData.path,
+          song_path: songData.path,
+        });
+
+      if (supabaseError) {
+        setisLoading(false);
+        return toast.error(supabaseError.message);
+      }
+
+      router.refresh();
+      setisLoading(false);
+      toast.success("Song created");
+      reset();
+      uploadModal.onClose();
     } catch (error) {
       toast.error("Something went wrong");
     } finally {
